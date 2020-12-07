b92aafeb8f1f0eba0f2678ffe74955df2ff57322
kevintram <thekevintram@gmail.com>
2020-12-07 16:54:13 -0500

    Switched to gameobjects and implemented bubble sort

---
+++ Assets/Scripts/CrowdController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Transforms;
+using Unity.Mathematics;
+
+public class CrowdController : MonoBehaviour {
+    public int population = 1000;
+    public float crowdDensity = 1f;
+
+    // i and j for manual bubble sort for loops
+    private int i = 0;
+    private int j = 0;
+
+    public List<PersonController> people;
+
+    public GameObject personPrefab;
+
+    public void Start() {
+        SpawnPeople(population);
+    }
+
+    public void SpawnPeople(int population) {
+        for (int i = 0; i < population; i++) {
+            //Initialize Positions
+            float rowOffset = (Mathf.Sqrt(population) / (2 * crowdDensity));
+            float x = ((i % Mathf.Sqrt(population)) / crowdDensity) - rowOffset;
+            float z = (i / (Mathf.Sqrt(population) * crowdDensity)) - rowOffset;
+            var position = new Vector3(x, 0, z);
+
+            var person = Instantiate(personPrefab, position, Quaternion.identity) as GameObject;
+            var controller = person.GetComponent<PersonController>();
+
+            //Set scale/value
+            var scale = UnityEngine.Random.Range(1f, 2f);
+            person.transform.localScale = new Vector3(
+                    scale,
+                    scale,
+                    scale
+            );
+            controller.value = scale;
+
+            people.Add(controller);
+        }
+        Debug.Log("");
+    }
+
+    public void FixedUpdate() {
+        bubbleSort();
+    }
+
+    private void bubbleSort() {
+        if (i < people.Count) {
+            if (j < people.Count - 1) {
+                if (people[j].value > people[j + 1].value) {
+                    // swap in list
+                    var personTemp = people[j];
+                    people[j] = people[j + 1];
+                    people[j + 1] = personTemp;
+                    // swap in game
+                    var positionTemp = people[j].transform.position;
+                    people[j].transform.position = people[j + 1].transform.position;
+                    people[j + 1].transform.position = positionTemp;
+                }
+                j++;
+            } else {
+                i++;
+                j = 0;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PersonController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonController : MonoBehaviour {
+
+    public float movementSpeed;
+
+    public float moveSpeedX;
+    public float moveSpeedZ;
+
+    public float value; // the value that represents the person, will be the same value as their scale
+
+    private SpriteRenderer sprite;
+
+    private void Awake() {
+        sprite = GetComponent<SpriteRenderer>();
+        moveSpeedX = Random.Range(-4f, 4f);
+        moveSpeedZ = Random.Range(-4f, 4f);
+    }
+}
