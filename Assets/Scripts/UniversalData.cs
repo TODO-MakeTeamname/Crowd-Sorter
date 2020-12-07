4aec5e8b7fc66249e12d2d29d79642823a420e33
kevintram <thekevintram@gmail.com>
2020-12-07 16:53:41 -0500

    Switched to gameobjects and implemented bubble sort

--- Assets/Scripts/UniversalData.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class UniversalData : MonoBehaviour
-{
-    public static int populationCount;
-    public static float crowdDensity;
-}
